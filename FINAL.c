c105e229b10639f6a3f2644e45bf4ee21a1651c9
Victor Almeida <victordealmeida5@gmail.com>
2019-11-30 04:24:39 -0300

    30-11-2019 04:21

--- FINAL.c
+++ FINAL.c
@@ -58,7 +58,7 @@ Cliente GetCliente(){ //FUNCAO PARA LER DADOS DO CLIENTES
 Carro GetCarro(){ //FUNCAO PARA LER DADOS DE CARROS
     Carro car;
     Cliente cli;
-    printf("MODEL: ");
+    printf("MODELO: ");
     setbuf(stdin,NULL);
     gets(&car.modelo);
     printf("COR: ");
@@ -153,45 +153,55 @@ void menu(){
                         printf("|-------------------------------|\n");
                         printf("| Digite o ID do Cliente: ");
                         scanf ("%d",&consultaID);
-                        int err = 0;
+                        int confirmaSeClienteExiste = 0; //SE 0 == CLIENTE NAO EXISTE  |  SE 1 == CLIENTE EXISTE.
 						Cliente cli;
 						Carro car;
-						int z;
-						FILE *arq_cliente;    //PONTEIRO PARA ARQUIVO
-						arq_cliente = fopen("arquivo_cliente.txt", "r"); //ABRIR ARQUIVO CLIENTE
-						while(fscanf(arq_cliente,"%s %s %s %d",cli.nome,cli.cpf,cli.endereco,&cli.idCli)!= EOF){ //LER TODAS AS LINHAS DO ARQUIVO
-                            if(consultaID == cli.idCli){    //COMPARAR OS IDs CADASTRADOS NO ARQUIVO CASO ENCONTRE O ID DESEJADO RETORNA OS DADOS ASSOCIADOS
-                                printf("\n=== DADOS CLIENTE ===\nNOME: %s\nCPF: %s\nENDERECO: %s\nID CLIENTE: %d\n",cli.nome,cli.cpf,cli.endereco,cli.idCli); //RETORNA OS DADOS
-                                err = 1;
-                                z = cli.idCli;
-                            }
-						}
-						if(err == 0){
-                                printf("\nCLIENTE NAO ENCONTRADO\n");
+						int auxIDCli; // AUXILIAR PARA GUARDA ID DO CLIENTE QUE FOI CONSULTADO.
+						FILE *arq_cliente;    //PONTEIRO PARA ARQUIVO.
+						arq_cliente = fopen("arquivo_cliente.txt", "r"); //ABRIR ARQUIVO CLIENTE.
+						while(fscanf(arq_cliente,"%s %s %s %d",cli.nome,cli.cpf,cli.endereco,&cli.idCli)!= EOF){ //LACO PARA LER TODAS AS LINHAS DO ARQUIVO
+                            if(consultaID == cli.idCli){//CONDICIONAL PARA COMPARAR OS IDs CADASTRADOS NO ARQUIVO CASO ENCONTRE O ID DESEJADO RETORNA OS DADOS ASSOCIADOS
+                                printf("\n=== DADOS CLIENTE ===\nNOME: %s\nCPF: %s\nENDERECO: %s\nID CLIENTE: %d\n",cli.nome,cli.cpf,cli.endereco,cli.idCli); //RETORNA OS DADOS DO CLIENTE DESEJADO
+                                confirmaSeClienteExiste = 1; //SE ENTROU NA CONDICIONAL SIGNIFICA QUE O CLIENTE EXISTE, VAREAVEL RECEBE 1.
+                                auxIDCli = cli.idCli; //GUARDANDO O ID DO CLIENTE CONSULTADO NA VAREAVEL AUXILIAR.
+                            } //FIM DA CONDICIONAL
+						}// FIM DO LACO.
+						if(confirmaSeClienteExiste == 0){//SO ENTRA NESSA CONDICAO CASO O CLIENTE NAO EXISTA, VALOR DA VAREAVEL == 0.
+                                printf("\nCLIENTE NAO ENCONTRADO\n");// PRINTA MENSAGEM
+                                system("pause");
+                                system("cls");
+                                break;//ENCERRA A CONTINUACAO DO PROGRAMA E VOLTA PARA O MENUR PRINCIPAL.
                         }
-                        int errc = 0;int nc=0;
-						FILE *arq_carro;
-						arq_carro = fopen("arquivo_carro.txt", "r");
-						if (err == 1){
+                        int confirmaSeCarroExiste = 0;// SE O == CARRO NÃO EXISTE  |  SE 1 == CARRO EXISTE.
+                        int numerosCarros = 1;//VAREAVEL PARA CONTAR O NUMEROS DE CARROS DO CLIENTE, INICIALMENTE RECEBE 1.
+						FILE *arq_carro; // PONTEIRO PARA ARQUIVO CARRO
+						arq_carro = fopen("arquivo_carro.txt", "r"); // ABRIR ARQUIVO CARRO
+						if (confirmaSeClienteExiste == 1){//CONDICIONAL PARA PERMITIR CADASTRAR UM CARRO APENAS SE EXISTIR UM CLIENTE DONO DESTE CARRO, PARA NÃO PERMITIR QUE SEJA CADASTRADO UM CARRO SEM UM CLIENTE ASSOCIADO A ELE.
                             while(fscanf(arq_carro,"%s %s %s %d %d",car.modelo,car.cor,car.placa,&car.idCar,&car.idCli)!= EOF){ //LER TODAS AS LINHAS DO ARQUIVO
-                                if(consultaID == car.idCli){    //COMPARAR OS IDs CADASTRADOS NO ARQUIVO CASO ENCONTRE O ID DESEJADO RETORNA OS DADOS ASSOCIADOS
-                                    nc++;
-                                    printf("\n=== DADOS DO CARRO %d ===\nMODELO: %s\nCOR: %s\nPLACA: %s\nID CARRO: %d\n",nc,car.modelo,car.cor,car.placa,car.idCar); //RETORNA OS DADOS
-                                    errc = 1;
-                                }
-                            }
-                            if(errc == 0){
-                                printf("\nCLIENTE NAO TEM CARRO CADASTRADO\n");
+                                if(consultaID == car.idCli){//CONDICIONAL PARA COMPARAR OS IDs CADASTRADOS NO ARQUIVO, CASO ENCONTRE O ID DESEJADO RETORNA OS DADOS ASSOCIADOS
+                                    printf("\n=== DADOS DO CARRO %d ===\nMODELO: %s\nCOR: %s\nPLACA: %s\nID CARRO: %d\n",numerosCarros,car.modelo,car.cor,car.placa,car.idCar); //RETORNA OS DADOS
+                                    numerosCarros++;//INCREMENTA NUMEROS DE CARROS CADASTRADOS.
+                                    confirmaSeCarroExiste = 1;//SE ENTROU NA CONDICIONAL SIGNIFICA QUE O CARRO EXISTE, VAREAVEL RECEBE 1.
+                                }//FIM DA CONDICIONAL
+                            }//FIM DO LACO
+                            if(confirmaSeCarroExiste == 0){//SO ENTRA NESSA CONDICAO CASO CARRO NAO EXISTA, VALOR DA VAREAVEL == 0.
+                                printf("\nCLIENTE NAO TEM CARRO CADASTRADO\n");//PRINTA MENSAGEM.
                             }
 						}
-						fclose(arq_cliente); //FECHAR ARQUIVO CLIENTE
-						fclose(arq_carro);
+						fclose(arq_cliente); //FECHAR ARQUIVO CLIENTE.
+						fclose(arq_carro);  //FECHAR ARQUIVO CARRO.
 						system("pause");
 						system("cls");
 
-						int escolha_carro;
+                            int escolha_carro;
+                            char mensagemCar[20];
                             do
                             {
+                            if(confirmaSeCarroExiste == 0){//SE O CLIENTE NÃO TIVER CARRO CADASTRADO A MENNSAGEM SERA "UM CARRO".
+                                strcpy(mensagemCar, "UM CARRO");//FUNCAO PARA COPIAR "STRING"(MESAGEM) PARA DENTRO DA VAREAVEL
+                            }else{// SE O CLIENTE TIVER UM CARRO CADASTRADO A MENSAGEM SERA "UM NOVO CARRO".
+                                strcpy(mensagemCar, "UM NOVO CARRO");//FUNCAO PARA COPIAR "STRING"(MENSAGEM) PARA DENTRO DA VAREAVEL
+                            }
                             printf("|-------------------------------|\n");
                             printf("|        CADASTRAR CARRO        |\n");
                             printf("|-------------------------------|\n");
@@ -200,30 +210,29 @@ void menu(){
                             printf("|-------------------------------|\n");
                             printf("|  2  |          NAO            |\n");
                             printf("|-------------------------------|\n");
-                            printf("DESEJA CADASTRAR UM NOVO CARRO PARA ESTE CLIENTE?: ");
+                            printf("DESEJA CADASTRAR %s PARA ESTE CLIENTE?: ",mensagemCar);//COLOCA A MENSAGEM ENTRE A PALAVRA
                             scanf("%d",&escolha_carro);
-                            //system("cls");
                             switch(escolha_carro){
                                 case 1:{
                                     printf("|-------------------------------|\n");
                                     printf("|        CADASTRAR CARRO        |\n");
                                     printf("|-------------------------------|\n");
                                     FILE *arq_carro;
-                                    arq_carro = fopen("arquivo_carro.txt", "a"); //ABRIR ARQUIVO CARRO
-                                    if(arq_carro ==  NULL){ //VERIFICAR SE OUVE ERRO AO ABRIR O ARQUIVO CARRO
-                                        printf("ERRO NA ABERTURA DO ARQUIVO!"); //RETORNAR MENSAGEM DE ERRO CASO OUVER ERRO QUANDO ABRIR O ARQUIVO
-                                        return 1;
+                                    arq_carro = fopen("arquivo_carro.txt", "a"); //ABRIR ARQUIVO CARRO.
+                                    if(arq_carro ==  NULL){ //VERIFICAR SE OUVE ERRO AO ABRIR O ARQUIVO CARRO.
+                                        printf("ERRO NA ABERTURA DO ARQUIVO!"); //RETORNAR MENSAGEM DE ERRO CASO OUVER ERRO QUANDO ABRIR O ARQUIVO.
                                     }
                                     Carro car;
                                     car = GetCarro();
-                                    car.idCli = z;
+                                    car.idCli = auxIDCli;// ID CLIENTE QUE TINHA SIDO GUARDADA PELO AUXILIAR E ASSOCIADO AO ID DE CLIENTE QUE CONTEM NA ESTRUTURA CARRO.
                                     fprintf(arq_carro,"%s %s %s %d %d\n",car.modelo,car.cor,car.placa,car.idCar,car.idCli); //GRAVAR DADOS DO "CAR" CARRO NO ARQUIVO CARRO
                                     fclose(arq_carro); //FECHAR ARQUIVO CARRO
                                     printf("Carro Cadastrado com Sucesso\n"); //MENSAGEM DE CONCLUSSAO
+                                    confirmaSeCarroExiste = 1;//DEPOIS DE CADASTRAR UM CARRO A VAREAVEL RECEBE 1.
                                     system("pause");
                                     system("cls");;
 
-                                }
+                                }//PAREI AQUI........................................................30/11/2019 04:21.
                                 break;
                             }
                             } while(escolha_carro!=2);
@@ -250,10 +259,17 @@ void menu(){
                         int x;
                         x = cli.idCli;
                         system("pause");
+                        system("cls");
 
-                            int escolha_carro;
+                            int escolha_carro,cont=0;
+                            char ncar[10];
                             do
                             {
+                            if( cont == 0){
+                                strcpy(ncar, "UM");
+                            }else{
+                                strcpy(ncar, "OUTRO");
+                            }cont++;
                             printf("|-------------------------------|\n");
                             printf("|        CADASTRAR CARRO        |\n");
                             printf("|-------------------------------|\n");
@@ -262,7 +278,7 @@ void menu(){
                             printf("|-------------------------------|\n");
                             printf("|  2  |          NAO            |\n");
                             printf("|-------------------------------|\n");
-                            printf("DESEJA CADASTRAR CARRO?: ");
+                            printf("DESEJA CADASTRAR %s CARRO PARA ESTE CLIENTE?: ",ncar);
                             scanf("%d",&escolha_carro);
                             system("cls");
                             switch(escolha_carro){
@@ -330,20 +346,40 @@ void menu(){
                         printf("| DIGITE O ID DO CLIENTE PARA EXCLUIR: ");
                         scanf ("%d",&excluirID);
                         Cliente cli;
-                        FILE *temp, *arq_cliente; //  ARQUIVO TEMPORARIO
+                        FILE *temp_cliente, *arq_cliente; //  ARQUIVO TEMPORARIO
 						if((arq_cliente = fopen("arquivo_cliente.txt", "rb")) == NULL);
-						if((temp = fopen("temp.txt","wb")) == NULL);
-// ABRIR ARQUIVO CARRO E CRIAR ARQUIVO TEMPcARRO;
-						// PASSA TODOS OS REGISTROS DO ARQUIVO CLIENTE PARA O ARQUIVO TEMP, EXCETO O REGISTRO COM O ID QUE SERA EXCLUIDO
-                        while(fscanf(arq_cliente,"%s %s %s %d",cli.nome,cli.cpf,cli.endereco,&cli.idCli) != EOF){
-                            if (excluirID != cli.idCli){
-                                fprintf(temp,"%s %s %s %d\n",cli.nome,cli.cpf,cli.endereco,cli.idCli);
+						if((temp_cliente = fopen("temp_cliente.txt","wb")) == NULL);
+
+                        Carro car;
+                        FILE *temp_carro, *arq_carro;
+                        if((arq_carro = fopen("arquivo_carro.txt","rb")) == NULL);
+                        if((temp_carro = fopen("temp_carro","wb")) == NULL);
+
+						// PASSA TODOS OS REGISTROS DOS ARQUIVOS PARA OS ARQUIVOS TEMPORARIOS, EXCETO O REGISTRO COM O ID QUE SERA EXCLUIDO
+                        while(fscanf(arq_cliente,"%s %s %s %d",cli.nome,cli.cpf,cli.endereco,&cli.idCli) != EOF){ //LISTANDO ARQUIVO_CLIENTE
+                            if (excluirID != cli.idCli){ //CASO ID QUE DESEJA EXCLUIR SEJA DIFERENTE DO ID DO CLIENTE...
+                                fprintf(temp_cliente,"%s %s %s %d\n",cli.nome,cli.cpf,cli.endereco,cli.idCli); //...PRINTAR DADOS CLIENTE NO ARQUIVO TEMP
                             }
                         }
+                        while(fscanf(arq_carro,"%s %s %s %d %d",car.modelo,car.cor,car.placa,&car.idCar,&car.idCli) != EOF){//LISTANDO ARQUIVO_CARRO
+                            if(excluirID != car.idCli){
+                                fprintf(temp_carro,"%s %s %s %d %d",car.modelo,car.cor,car.placa,car.idCar,car.idCli);
+                            }
+                        }
+
+                        /*   FECHAR ARQUIVOS ABERTOS     */
                         fclose(arq_cliente);
-                        fclose(temp);
-                        remove("arquivo_cliente.txt"); //EXCLUIR ARQUIVO CLIENTE
-                        rename("temp.txt", "arquivo_cliente.txt"); // RENOMEAR ARQUIVO TEMP PARA ARQUIVO CLIENTE
+                        fclose(temp_cliente);
+                        fclose(arq_carro);
+                        fclose(temp_carro);
+
+                        remove("arquivo_cliente.txt"); //EXCLUIR ARQUIVO_CLIENTE
+                        rename("temp_cliente.txt", "arquivo_cliente.txt"); // RENOMEAR ARQUIVO TEMP_CLIENTE PARA ARQUIVO_CLIENTE
+                        remove("arquivo_carro.txt"); //EXCLUIR ARQUIVO_CARRO
+                        rename("temp_carro.txt", "arquivo_carro.txt"); // RENOMEAR ARQUIVO TEMP_CARRO PARA ARQUIVO_CARRO
+                        printf("CLIENTE EXCLUIDO!");
+                        system("pause");
+                        system("cls");
                     }
                     break;
                     case 6:{
